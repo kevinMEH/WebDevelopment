7bf50d54d5183ead82730c2b85f05aac326de4e2
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-11-02 10:31:03 -0400

    Changed styles

--- Projects/sliding-game/src/components/Grid/Grid.styles.js
+++ Projects/sliding-game/src/components/Grid/Grid.styles.js
@@ -5,12 +5,7 @@ export const Wrapper = styled.div`
     gap: 0.5rem;
 
     grid-template-rows: repeat(4, 1fr);
-    grid-template-columns: repeat(5, 1fr);
-    grid-template-areas:
-        "0 1 2 3"
-        "4 5 6 7"
-        "8 9 10 11"
-        "12 13 14 15";
+    grid-template-columns: repeat(4, 1fr);
 
     box-sizing: inherit;
 `
@@ -18,6 +13,5 @@ export const Wrapper = styled.div`
 export const BlockWrapper = styled.div`
     position: relative;
     box-sizing: inherit;
-    ${'' /* grid-area: REPLACEME; */}
     display: block;
 `
