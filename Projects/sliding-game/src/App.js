ef7dd74810e15ba5fd8d2002d0e46842a158f6f9
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-11-02 10:31:51 -0400

    Fixed App return value

--- Projects/sliding-game/src/App.js
+++ Projects/sliding-game/src/App.js
@@ -6,16 +6,18 @@ import Grid from "./components/Grid";
 import { GlobalStyle } from "./GlobalStyle";
 
 const App = () => {
-	<>
-		<h1>Welcome to the Sliding Game!</h1>
-		<p>
-			To win the game, arrange the numbers from 1 to 15 in order from left
-			to right and top to bottom. Drag and drop the numbers to rearrange
-			the numbers.
-		</p>
-		<Grid />
-		<GlobalStyle />
-	</>;
+	return (
+		<>
+			<h1>Welcome to the Sliding Game!</h1>
+			<p>
+				To win the game, arrange the numbers from 1 to 15 in order from left
+				to right and top to bottom. Drag and drop the numbers to rearrange
+				the numbers.
+			</p>
+			<Grid />
+			<GlobalStyle />
+		</>
+	);
 };
 
 export default App;
