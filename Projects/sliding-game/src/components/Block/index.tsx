32fc636c7b1d35f6e57dc645494aae2e765860ef
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-11-03 16:54:24 -0400

    Made empty block have empty text

--- Projects/sliding-game/src/components/Block/index.tsx
+++ Projects/sliding-game/src/components/Block/index.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 import { Content } from "./Block.styles";
 
 const Block = ( {blockClassName, text}: {blockClassName: string, text: string} ) => {
+    if(blockClassName.includes("isEmpty")) text = "";
     return (
-        <span>
+        <span className={blockClassName}>
             <Content>
                 <h1>{text}</h1>
             </Content>
