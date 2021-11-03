cd8d8abc3ec3f8fd8b95f69c161a9921cb0447fe
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-11-02 21:20:36 -0400

    Moved swappable logic to grid component

--- Projects/sliding-game/src/components/swappable.ts
+++ Projects/sliding-game/src/components/swappable.ts
@@ -1,4 +1,4 @@
-import { Plugins, Swappable } from "@shopify/draggable";
+import { Swappable } from "@shopify/draggable";
 
 const initializeSwappable = () => {
 	console.log("Initializing");
@@ -15,32 +15,10 @@ const initializeSwappable = () => {
 		mirror: {
 			appendTo: containerSelector,
 			constrainDimensions: true,
-		},
-		plugins: [Plugins.ResizeMirror],
+		}
 	});
 
 	console.log("Swappable Success.");
-	
-	let sourceContainer: HTMLElement;
-	
-	swappable.on("drag:start", (event) => {
-		sourceContainer = event.source;
-	})
-	
-	swappable.on("swappable:swap", (event) => {
-		// User is using empty block
-		if(sourceContainer.classList.contains("Block--isEmpty")) return;
-		
-		// User is using numbered block and swapping with empty
-		if(event.over.classList.contains("Block--isEmpty")) return;
-		
-		// Else if user is swapping numbered with numbered cancel.
-		event.cancel();
-	});
-	
-	// swappable.on("swappable:swapped", (event) => {
-	// 	event.swappedElement.
-	// })
 
 	swappable.on("swappable:start", () => console.log("swappable:start"));
 	swappable.on("swappable:swapped", () => console.log("swappable:swapped"));
